658224bb2ea4fe2b2b2ef7b5d4ac0cebe3a56767
DevXChardike <chardikeltd@gmail.com>
2023-02-15 16:13:55 +0600

    Fetch json data from public directory

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Home = () => {
+
+
+  // Fetch json data from public directory 
+  useEffect(() => {
+    fetch('products.json')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
       <h1>This is home page</h1>
