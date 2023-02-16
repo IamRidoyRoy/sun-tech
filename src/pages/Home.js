947c813f60ab8cfb933b03cdd4ff1a88e5f93b91
DevXChardike <chardikeltd@gmail.com>
2023-02-16 14:31:26 +0600

    Show top ratted product on top ratted page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,31 @@ import ProductCard from "../components/ProductCard";
 const Home = () => {
 
 
-  const { state: { products } } = useProducts();
+  const { state: { products, loading, error } } = useProducts();
+
+  let content;
+
+  if (loading) {
+    content = <p>It's Loading Baby!</p>
+
+  }
+
+  if (error) {
+    content = <p>Occuers an Error!</p>
+  }
+
+  if (!error && !loading && products.length === 0) {
+    content = <p>There is no products to show!</p>
+  }
+
+  if (!error && !loading && products.length) {
+    content = products.map((product) => <ProductCard key={product._id} product={product}></ProductCard>)
+  }
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
 
-      {products.map((product) => <ProductCard product={product}></ProductCard>)}
+      {content}
 
     </div>
   );
--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { useProducts } from "../components/context/ProductProvider";
+import ProductCard from "../components/ProductCard";
 
 const TopRated = () => {
+
+  const { state: { products, loading, error } } = useProducts();
+
+  let content;
+
+  if (loading) {
+    content = <p>It's Loading Baby!</p>
+
+  }
+
+  if (error) {
+    content = <p>Occuers an Error!</p>
+  }
+
+  if (!error && !loading && products.length === 0) {
+    content = <p>There is no products to show!</p>
+  }
+
+  // Add filter option for showing top ratted product. 
+  if (!error && !loading && products.length) {
+    content = products.filter((productf) => productf.rating >= 4).map((product) => <ProductCard key={product._id} product={product}></ProductCard>)
+  }
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is featured page</h1>
+    <div >
+      <h2 className="flex justify-center text-2xl mt-6">Top Rated Products</h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+        {content}
+
+      </div>
     </div>
   );
 };
