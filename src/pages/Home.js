04e1270b2d4d6136d82635e04b1cf1689b9da73f
DevXChardike <chardikeltd@gmail.com>
2023-02-16 11:48:49 +0600

    Call the products state and map the products, send it to ProductCard

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,13 @@ import ProductCard from "../components/ProductCard";
 const Home = () => {
 
 
-  // console.log(data);
+  const { state: { products } } = useProducts();
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+
+      {products.map((product) => <ProductCard product={product}></ProductCard>)}
+
     </div>
   );
 };
